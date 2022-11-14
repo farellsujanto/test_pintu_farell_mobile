606de8194db46e022b1922b15cf6c0d3b99018d0
farells <f.sujanto@gmail.com>
2022-11-15 00:36:31 +0700

    Feat: Add MarketListScreen and NotFoundScreen

---
+++ src/screens/MarketListScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, } from 'react-native';
+import styles from './styles';
+
+const MarketListScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>MarketListScreen</Text>
+        </View>
+    );
+}
+
+export default MarketListScreen;
---
+++ src/screens/NotFoundScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, } from 'react-native';
+import styles from './styles';
+
+const NotFoundScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Not Found</Text>
+        </View>
+    );
+}
+
+export default NotFoundScreen;
