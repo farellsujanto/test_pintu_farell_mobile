cfbdfce02d9bf42b91fe179ea7aa2030078ab788
farells <f.sujanto@gmail.com>
2022-11-15 00:37:18 +0700

    Feat: Add route constants

---
+++ src/constants/routeNames.ts
@@ -0,0 +1,5 @@
+export const HOME_SCREEN_ROUTE = 'Home';
+export const DISCOVER_SCREEN_ROUTE = 'Discover';
+export const MARKET_SCREEN_ROUTE = 'Market';
+export const WALLET_SCREEN_ROUTE = 'Wallet';
+export const ACCOUNT_SCREEN_ROUTE = 'Account';
