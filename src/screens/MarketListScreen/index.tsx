e5329c2b3ffecd3bf853f7a14d5053142a3d8b8c
farells <f.sujanto@gmail.com>
2022-11-16 00:07:24 +0700

    Improve: Integrate MarketListScreen with components to match UI design

--- src/screens/MarketListScreen/index.tsx
+++ src/screens/MarketListScreen/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Text, View, } from 'react-native';
+import { View } from 'react-native';
+import FilterChipButton from './components/FilterChipButton';
+import HeaderSection from './components/HeaderSection';
+import MarketChangesList from './components/MarketChangesList';
 import styles from './styles';
 
 const MarketListScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>MarketListScreen</Text>
+            <HeaderSection />
+            <View style={styles.filterChipsContainer}>
+                <FilterChipButton label='New' iconName='flower-outline' />
+                <FilterChipButton label='DeFi' iconName='business-outline' />
+                <FilterChipButton label='NFT/Gaming' iconName='game-controller-outline' />
+                <FilterChipButton label='CEX' iconName='swap-horizontal' />
+                <FilterChipButton label='DEX' iconName='swap-horizontal' />
+            </View>
+            <MarketChangesList />
         </View>
     );
 }
--- src/screens/MarketListScreen/styles.tsx
+++ src/screens/MarketListScreen/styles.tsx
@@ -1,9 +1,18 @@
 import { StyleSheet } from 'react-native';
+import { whiteColor } from '../../themes/colors';
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: whiteColor,
+    },
+    spacer: {
+        flex: 1,
+    },
+    filterChipsContainer: {
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
     },
 });
