baa2370c5b72318879a03eccd8a245ddbd0add18
farells <f.sujanto@gmail.com>
2022-11-16 00:04:16 +0700

    Improve: Add green & red colors

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,2 +1,5 @@
 export const blackColor = '#171717';
 export const whiteColor = '#ffffff';
+export const greenColor = '#25a764';
+export const redColor = '#e54040';
+export const greyColor = '#c1c1c1';
