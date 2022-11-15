5247bc483be6289b49231f08215b24cbb8e2368f
farells <f.sujanto@gmail.com>
2022-11-16 00:25:11 +0700

    Improve: Setup rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,14 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.js', '*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'prettier/prettier': 0,
+        'jsx-quotes': [2, 'prefer-single'],
+        'semi': [0, 'never'],
       },
     },
   ],
